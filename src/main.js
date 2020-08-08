6bf06704df4ae0b3005006ffe7e189b880e7ec78
jaisoneji <jai.soneji@gmail.com>
2020-08-08 21:22:49 +0530

    Added PWA feature

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/main.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
