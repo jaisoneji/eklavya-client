8e962c9b43dbf7e575b33e7d23a1506b11349aff
hrithik20 <hmalvani5@gmail.com>
2020-09-06 22:36:54 +0530

    login and registraion ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,24 @@
 <template>
 <div class="px-0 py-5 flex flex-col w-screen h-full bg-background-primary md:justify-around md:content-center md:items-center ">
+
   <Loader v-if="isLoading"/>
+  
   <div v-else class="px-0 py-5 flex flex-col w-screen h-full bg-background-primary md:justify-around md:content-center md:items-center md:px-0">
+  
     <div  class=" px-5 py-5 w-full h-5/6 mt-20 md:mt-2 md:w-1/3 bg-gray-200 rounded-lg my-4 items-center md:flex-row md:content-center md:items-center">
   
             <h1 class="font-hairline mb-8 text-background-secondary text-center text-5xl md:justify-around md:content-center md:items-center ">Login</h1>
+  
             <div class="border-teal p-8 border-t-12 bg-white mb-8 rounded-lg shadow-lg md:h-4/5">
+  
                 <div class="mb-4">
-                    <label class="font-bold text-black block mb-2 ">Email</label>
-                    <input required type="email" v-model="email" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Your Username">
+                    <!-- <label class="font-bold text-black block mb-2 ">Email</label> -->
+                    <input required type="email" v-model="email" class="border rounded w-full py-2 px-4 outline-none focus:shadow-outline " placeholder="Your Username">
                 </div>
 
                 <div class="mb-4">
-                    <label class="font-bold text-black block mb-2">Password</label>
-                    <input required type="password" v-model="password" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Your Password">
+                    <!-- <label class="font-bold text-black block mb-2">Password</label> -->
+                    <input required type="password" v-model="password" class="border rounded w-full py-2 px-4 outline-none focus:shadow-outline" placeholder="Your Password">
                 </div>
 
                 <div class="flex items-center justify-between">
@@ -29,7 +34,7 @@
                 
             </div>
             <div class="text-center">
-                <p class="text-black text-sm">Don't have an account? <a href="#" class="no-underline text-blue font-bold">Create an Account</a>.</p>
+                <p class="text-black text-sm">Don't have an account? <a href="Register" class="no-underline text-blue font-bold">Create an Account</a>.</p>
             </div>
         </div>
     
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,18 +1,19 @@
 <template>
 <!-- main background -->
-<div class="h-full bg-background-primary">
+<div class="px-0 py-5 flex flex-col w-screen h-full bg-background-primary md:justify-around md:content-center md:items-cente">
 <!-- first div -->
-    <div class="flex items-center justify-center h-full">
+    <div class="px-0 py-5 flex flex-col w-screen h-full bg-background-primary md:justify-around md:content-center md:items-center md:px-0">
 
-      <div class="flex max-w-3xl">
+      <div class="px-5 py-5 w-full h-5/6 mt-20 md:mt-2 md:w-1/3  rounded-lg my-4 items-center md:flex-row md:content-center md:items-center">
 
         <div class="bg-blue-100 rounded-lg w-auto p-8 ">
 
 <!-- heading -->
-          <h1 class="text-blue-700 text-3xl text-center mb-4">Registration</h1>
-            <div class="my-3">
-              <input v-model="name" type="text" placeholder="Username" class="border rounded w-full py-2 px-4 outline-none focus:shadow-outline" />
-            </div>
+          <h1 class="font-hairline mb-8 text-background-secondary text-center text-5xl md:justify-around md:content-center md:items-center ">Registration</h1>
+            <div class="border-teal p-8 border-t-12 bg-white mb-8 rounded-lg shadow-lg md:h-4/5">
+            <div class="mb-4">
+                    <input required type="email" v-model="email" class="border rounded w-full py-2 px-4 outline-none focus:shadow-outline " placeholder="Your Username">
+                </div>
             <div class="my-3">
                 <input v-model="email" type="email" placeholder="Email" class="border rounded w-full py-2 px-4 outline-none focus:shadow-outline" />
             </div>
@@ -20,11 +21,12 @@
                 <input v-model="password" type="password" placeholder="Password" class="border rounded w-full py-2 px-4 outline-none focus:shadow-outline" />
               </div>
             <div class="my-6 flex">
-              <button class="border rounded w-1/2 border-blue-700 md:w-full py-2" @click.prevent="register">REGISTER</button>
+              <button class="border rounded w-1/2 border-background-primary md:w-full py-2 hover:bg-background-secondary hover:text-white text-black font-bold py-2 px-4 rounded outline-none" @click.prevent="register">REGISTER</button>
               <router-link :to="Login" class="border rounded w-1/2 md:w-full py-2 border-blue-700 bg-blue-600 text-white ml-2">SIGN IN ?</router-link>
             </div>
           </div>
         </div>
+      </div>
         <!-- <div class="lg:w-1/2 ">
           <img src="../assets/architecture-1869211_1280.jpg" alt="" class="w-full h-full  object-cover" />
         </div> -->
