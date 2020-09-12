fa0d1267edfc4d4ca2ad62d5c6bc52853664d8ab
unknown <soniaat99@gmail.com>
2020-09-12 16:34:02 +0530

    semi fixed profile page

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,87 +2,96 @@
 <div class="flex w-screen h-screen bg-background-primary md:justify-center md:content-center md:align-center md:items-center" :class="theme">
     <ProfileError @hideError="hideError()" v-if="toggleError==false" />
     <div v-else class="flex w-screen h-full bg-background-primary md:justify-center md:content-center md:align-center md:items-center " :class="theme">
-      
-      <div class=" flex flex-col md:flex-row md:justify-center md:content-center md:align-center md:items-center ">
-          <!-- Form div starts here -->
-      <div class=" px-5 md:py-10 w-full h-5/6 mt-20 md:mt-10 md:w-1/3  my-1 items-center md:flex-row md:content-center md:items-center">
-        <!-- <div class="flex items-center text-white ">
-            Full Name:
-        </div>
-            <div class="border-b border-white py-1">
-                <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" aria-label="Full name">
-            </div> -->
-            <div class="flex flex-col">
-                <span class="text-white">Register As:</span>
-                <div class="mt-5">
-                    <label class="inline-flex items-center text-white">
-                    <input type="radio" class="form-radio" name="Registeras" value="student">
-                    <span class="ml-2">Student</span>
-                    </label>
-                    <label class="inline-flex items-center text-white ml-16">
-                    <input type="radio" class="form-radio" name="Registeras" value="teacher">
-                    <span class="ml-2">Teacher</span>
-                    </label>
+        <div class="flex flex-col md:w-1/2 w-full md:h-screen md:flex-row md:justify-center md:content-center md:align-center md:items-center ">
+            <!-- Form div starts here -->
+            <div class=" mt-10 ml-5  md:h-screencontent-center align-center flex  justify-center items-center flex-col rounded-lg  w-11/12 h-132 bg-background-secondary md:bg-background-secondary md:w-1/2 md:h-132"   :class="theme">
+                        
+                <!-- Register as radio button -->
+                <div class="flex flex-row ">
+                    <span class="text-white font-bold" :class="theme">Register As:</span>
+                        <label class="inline-flex items-center align-center text-white ml-5">
+                        <input type="radio" class="form-radio" name="Registeras" value="student">
+                        <span class="ml-2">Student</span>
+                        </label>
+                        <label class="inline-flex items-center text-white ml-16">
+                        <input type="radio" class="form-radio" name="Registeras" value="teacher">
+                        <span class="ml-2">Teacher</span>
+                        </label>
                 </div>
+                <!--  Register as radio button ends -->
+                        
+                <div class="flex flex-col w-full items-center mt-6  ">            
+                    <!-- Roll no div -->
+                    <div :class="theme" class="flex flex-row w-9/12 justify-center items-center">
+                                <input v-model="rollno" required type="rollno" class="flex-1 border rounded-full w-full py-2 px-6 outline-none focus:shadow-outline " placeholder="Roll No">
+                    </div>
+                    <!-- Roll no div -->
+
+                    <div class=" flex flex-row">
+                        <!-- sem -->
+                        <div class="mt-10 px-3 inline-block relative w-18">
+                            <select class=" text-background-secondary appearance-none bg-white border border-white px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" :class="theme">
+                                <option>SEM</option>
+                                <option>Option 2</option>
+                                <option>Option 3</option>
+                            </select>
+                                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 text-background-secondary" :class="theme">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                            </div>
+                        </div>
+                        <!-- sem ends -->
+
+                        <!-- dept -->
+                        <div class="mt-10 px-3 inline-block relative w-18">
+                            <select class=" text-background-secondary appearance-none bg-white border border-white px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" :class="theme">
+                                <option>DEPT</option>
+                                <option>Option 2</option>
+                                <option>Option 3</option>
+                            </select>
+                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 text-background-secondary" :class="theme">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                            </div>
+                        </div>
+                        <!-- dept ends-->
+
+                        <!-- class -->
+                        <div class="mt-10 px-3 inline-block relative w-18">
+                            <select  class=" text-background-secondary appearance-none bg-white border border-white px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" :class="theme">
+                                <option>CLASS</option>
+                                <option>Option 2</option>
+                                <option>Option 3</option>
+                            </select>
+                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-background-secondary" :class="theme">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                            </div>
+                        </div>
+                        <!-- class ends-->
+                    </div>
+
+                    <!-- Mobile no -->
+                    <div :class="theme" class="flex flex-row w-9/12 justify-center items-center mt-6">
+                                <input v-model="mobileno"  required type="mobileno" class="border rounded-full w-full py-2 px-4 outline-none focus:shadow-outline " placeholder="Mobile Number">
+                    </div>
+                    <!-- Mobile no ends-->
+
+                    <!-- confirm button -->
+                    <div :class="theme" @click.prevent="register()" class="mt-6 flex flex-row border bg-white rounded-full w-1/2 h-12 justify-center items-center  transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-60 transition ease-in-out duration-300 hover:shadow-outline ">
+                        <button class="flex font-bold text-xl text-text-btn rounded outline-none align-center " :class="theme">Confirm</button>
+                        <img src="@/assets/tick.png" class="h-8 w-8 ml-4 align-center color-white  color-white" />
+                    </div>
+                    <!-- confirm button ends-->
+                </div>
+
             </div>
-        <div class="flex items-center text-white mt-5 md:mt-10">
-            Roll No.
-        </div>
-            <div class="border-b border-white py-1">
-                <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" aria-label="Full name">
-            </div>
+            <!-- Form div ends here -->
         
-        <div class="mt-10 p-2 inline-block relative w-18">
-            <select class=" text-white appearance-none bg-background-primary border border-white px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" :class="theme">
-                <option>SEM</option>
-                <option>Option 2</option>
-                <option>Option 3</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 text-white">
-                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
-        </div>
-        <div class="mt-10 p-2 inline-block relative w-18">
-            <select class=" text-white appearance-none bg-background-primary border border-white px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" :class="theme">
-                <option>DEPT</option>
-                <option>Option 2</option>
-                <option>Option 3</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 text-white">
-                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
-        </div>
-       <div class="mt-10 p-2 inline-block relative w-18">
-            <select class=" text-white appearance-none bg-background-primary border border-white px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" :class="theme">
-                <option>CLASS</option>
-                <option>Option 2</option>
-                <option>Option 3</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-white">
-                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
-        </div>
-        <div class="flex items-center text-white mt-5">
-        Mobile Number:
-        </div>
-        <div class="border-b border-white py-1">
-            <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" aria-label="Full name">
-        </div>
-        <div class="flex justify-end ">        
-            <div class="mt-3 w-1/3 p-2">
-                    <button class="border border-white text-white font-bold py-1 px-4 rounded" >
-                            Confirm
-                    </button>
-            </div>         
+            <!-- Image div -->
+            <div class=" flex items-center  md:m-auto md:block md:w-1/2 md:h-11/12  hidden ">
+                <img src="@/assets/clip-sign-up.png" class=" flex md:w-3/4 m-auto " />
+            </div> 
+            <!-- ends here    -->
         </div>
     </div>
-    <!-- Image div -->
-    <div class=" w-full md:w-1/2 md:align-center md:pl-6" >
-        <img src="@/assets/clip-sign-up.png"/>   
-    </div>   
-    <!-- ends here    -->
-  </div>
-</div>
 </div>
 </template>
 
