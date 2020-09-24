e2d24c27b2110f16475fc65ea31adfe717197554
unknown <soniaat99@gmail.com>
2020-09-24 18:29:00 +0530

    sidebar added

--- src/components/Dash.vue
+++ src/components/Dash.vue
@@ -1,13 +1,22 @@
 <template>
   <!-- main div with the theme -->
   <div class="flex justify-center align-center items-center content-center w-screen h-screen bg-background-primary" :class="theme">
-    <sidebar-menu :menu="menu"
-        :class="theme"
-        :to="href" />
+    <!-- sidebar start -->
+    <sidebar-menu
+        :menu="menu"
+        :collapsed="collapsed"
+        :theme="selectedTheme"
+        :show-one-child="true"
+        @toggle-collapse="onToggleCollapse"
+        @item-click="onItemClick"
+        :to="href"
+      />
+      <!-- sidebar start ends-->
         <router-view />
         <Classroom/>
         <Quizzes></Quizzes>
         <Scheduled></Scheduled>
+        <!-- Main right contents of the dashboard -->
         <!-- container div of clasroom, quizzes and schedule -->
         <div class="border flex flex-col w-1/2 ">
             <!-- classroom -->
@@ -34,7 +43,12 @@
   <!-- main div with the theme ends-->
 </template>
 <script>
+
 import { SidebarMenu } from 'vue-sidebar-menu'
+
+const separator = {
+
+}
 export default {
   data() {
             return {
@@ -45,30 +59,66 @@ export default {
                         hiddenOnCollapse: true
                     },
                     {
-                        href:'/EmailError',
-                        title: 'EmailError',
-                        icon: 'fa fa-user'
-                    },
-                    {
-                        href: '/Login',
-                        title: 'Login',
-                        icon: 'fa fa-chart-area',
-                        child: [
-                            {
-                                href: '/charts/sublink',
-                                title: 'Sub Link'
-                            }
-                        ]
-                    },
+                        header:'true',
+                        title: 'circle profiler',
                     
+                    },
                     {
-                        href: '/charts',
+                        component: separator
+                    },
+                    {
+                        header: true,
+                        title: 'Classroom',
+                        hiddenOnCollapse: true
+                    },
+                    // quizes start
+                    {
+                        header: true,
+                        title: 'Quizes',
+                        hiddenOnCollapse: true
+                    },
+                    {
+                        href:'',
+                        title: 'Create Quiz',
+                        icon: 'fa fa-list-alt'
+                    },
+                    {
+                        href:'',
+                        title: 'Blank Quiz',
+                        icon: 'fa fa-list-alt'
+                    },
+                    // quizes ends
+                    // schedule start
+                    {
+                        header: true,
+                        title: 'Scheduled',
+                        hiddenOnCollapse: true
+                    },
+                    // scedule ends
+                    // settings
+                    {
+                        href: '',
                         title: 'Query',
-                        icon: 'fa fa-chart-area',
+                        icon: 'fa  fa-question-circle',
+                    },
+                    {
+                        href:'',
+                        title: 'Settings',
+                        icon: 'fa fa-gears '
                     },
                 ],
                 collapsed: false,
-                selectedTheme: '',
+                themes: [
+                    {
+                    name: 'Default theme',
+                    input: ''
+                    },
+                    {
+                    name: 'White theme',
+                    input: 'white-theme'
+                    }
+                ],
+                selectedTheme: 'Default theme   ',
                 isOnMobile: false
             }
         },
@@ -102,3 +152,14 @@ computed:{
     }
 }
 </script>
+<style >
+
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600');
+.v-sidebar-menu .vsm-arrow:after{font-family: FontAwesome}
+
+.v-sidebar-menu .collapse-btn:after {
+content: "\f07e";
+font-family: FontAwesome;
+}
+
+</style>
