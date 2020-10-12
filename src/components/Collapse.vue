6cc6d0fb671e04630073881c629fd47217f96850
hrithik20 <hmalvani5@gmail.com>
2020-10-12 20:36:07 +0530

    tabular UI

--- src/components/Collapse.vue
+++ src/components/Collapse.vue
@@ -1,41 +1,56 @@
 <template>
-<div class="ml-8 border-red">
-    <div class="w-full md:w-10/12 mx-auto p-8">
-         <h1 class="text-text-text text-4xl" :class="theme">Quiz</h1>
-         <div class="shadow-md">
-            <div class="tab w-full overflow-hidden border-t">
-               <input class="absolute opacity-0 " id="tab-multi-one" type="radio" name="tabs">
-               <label class="block p-5 leading-normal cursor-pointer text-text-text" :class="theme" for="tab-multi-one">Scrape</label>
-               <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  <Scrape/>
-               </div>
+  <div class="flex flex-wrap">
+    <div class="w-full">
+      <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-text bg-white': openTab !== 1, 'text-white bg-background-secondary': openTab === 1}">
+            Profile
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-text bg-white': openTab !== 2, 'text-white bg-background-secondary': openTab === 2}">
+            Settings
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(3)" v-bind:class="{'text-text bg-white': openTab !== 3, 'text-white bg-background-secondary': openTab === 3}">
+            Options
+          </a>
+        </li>
+      </ul>
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+        <div class="px-4 py-5 flex-auto">
+          <div class="tab-content tab-space">
+            <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+              <p>
+                <!-- </Scrape> -->
+              </p>
             </div>
-            <div class="tab w-full overflow-hidden border-t">
-               <input class="absolute opacity-0" id="tab-multi-two" type="radio" name="tabs">
-               <label class="block p-5 leading-normal cursor-pointer text-text-text" :class="theme" for="tab-multi-two">Repository</label>
-               <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  
-               </div>
+            <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+              
+              <!-- </Scrape> -->
             </div>
-            <div class="tab w-full overflow-hidden border-t border-b">
-               <input class="absolute opacity-0" id="tab-multi-three" type="radio" name="tabs">
-               <label class="block p-5 leading-normal cursor-pointer text-text-text" :class="theme"  for="tab-multi-three">Scan your media files here</label>
-               <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  
-               </div>
+            <div v-bind:class="{'hidden': openTab !== 3, 'block': openTab === 3}">
+              <!-- </Scrape> -->
             </div>
-         </div>
+          </div>
+        </div>
       </div>
-      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Scrape from '@/components/Scrape.vue';
+// import Scrape from '@/components/Scrape.vue';
+
+
+
 export default {
    name:'Collapse',
     components:{
-      Scrape
+      // Scrape
     },
+ 
      computed:{
     theme(){
         if(this.$store.getters.getMode === 'theme-dark'){
@@ -45,66 +60,19 @@ export default {
             return 'theme-light'
         }
         },
-     }
+           data() {
+    return {
+      openTab: 1
+    }
+  },
+
+        methods: {
+    toggleTabs: function(tabNumber){
+      this.openTab = tabNumber
+    }
+  }
+       
+ }
 }
 </script>
 
-<style>
-         /* Tab content - closed */
-         .tab-content {
-         max-height: 0;
-         -webkit-transition: max-height .35s;
-         -o-transition: max-height .35s;
-         transition: max-height .35s;
-         }
-         /* :checked - resize to full height */
-         .tab input:checked ~ .tab-content {
-         max-height: 100vh;
-         }
-         /* Label formatting when open */
-         .tab input:checked + label{
-         /*@apply text-xl p-5 border-l-2 border-indigo-500 bg-gray-100 text-indigo*/
-         font-size: 1.25rem; /*.text-xl*/
-         padding: 1.25rem; /*.p-5*/
-         border-left-width: 2px; /*.border-l-2*/
-         border-color: #6574cd; /*.border-indigo*/
-         background-color: #f8fafc; /*.bg-gray-100 */
-         color: #6574cd; /*.text-indigo*/
-         }
-         /* Icon */
-         .tab label::after {
-          
-         float:right;
-         right: 0;
-         top: 0;
-         display: block;
-         width: 1.5em;
-         height: 1.5em;
-         line-height: 1.5;
-         font-size: 1.25rem;
-         text-align: center;
-         -webkit-transition: all .35s;
-         -o-transition: all .35s;
-         transition: all .35s;
-         }
-         /* Icon formatting - closed */
-         .tab input[type=radio] + label::after {
-         content: "\25BE";
-         font-weight:bold; /*.font-bold*/
-         border-width: 1px; /*.border*/
-         border-radius: 9999px; /*.rounded-full */
-         border-color: #b8c2cc; /*.border-grey*/
-         }
-         
-         /* Icon formatting - open */
-         .tab input[type=radio]:checked + label::after {
-         transform: rotate(315deg);
-         background-color: #6574cd; /*.bg-indigo*/
-         color: #f8fafc; /*.text-grey-lightest*/
-         }
-         .tab input[type=radio]:checked + label::after {
-         transform: rotateX(180deg);
-         background-color: #6574cd; /*.bg-indigo*/
-         color: #f8fafc; /*.text-grey-lightest*/
-         }
-      </style>
